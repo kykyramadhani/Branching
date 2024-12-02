d7d072e7159aaaef8a0011e1391cd83f24daef65
F1D022094_Rizky Insania Ramadhani <rizkyinsania26@gmail.com>
2024-12-02 17:31:56 +0800

    tes3

--- Tes/src/App.java
+++ Tes/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("djvbhdbv");
+        System.out.println("hYYYYYYY jamEtzzZ");
     }
 }
